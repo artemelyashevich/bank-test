bd4ac0035a4f24001c85b8b277c21446472d102a
artemelyashevich <elyashevich.artem@gmail.com>
2025-04-29 18:14:09 +0300

    Fixes

---
+++ src/main/java/com/elyashevich/bank/config/SchedulerConfig.java
@@ -0,0 +1,18 @@
+package com.elyashevich.bank.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import java.util.concurrent.Executors;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig implements SchedulingConfigurer {
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.setScheduler(Executors.newScheduledThreadPool(5));
+    }
+}
